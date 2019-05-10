6de78f90b622d889204930585ae48318447f8b3e
“Ryan <ryancarlile@gmail.com>
2019-05-09 18:45:01 -0700

    add resume branch

---
+++ js/resume.js
@@ -0,0 +1,53 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+  
+  var $imageSrc;  
+  $('.gallery img').click(function() {
+      $imageSrc = $(this).data('bigimage');
+  });
+  console.log($imageSrc);
+    
+    
+    
+  // when the modal is opened autoplay it  
+  $('#myModal').on('shown.bs.modal', function (e) {
+      
+  // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+
+  $("#image").attr('src', $imageSrc  ); 
+  })
+    
+    
+  // reset the modal image
+  $('#myModal').on('hide.bs.modal', function (e) {
+      // a poor man's stop video
+      $("#image").attr('src',''); 
+  }) 
+      
+    
+
+})(jQuery); // End of use strict
